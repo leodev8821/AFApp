eaf7e03b6c69acddd417cb570769584d09d3170c
leodev8821 <jaime.leo.caicedo@gmail.com>
2024-03-13 12:37:08 +0100

    Added all database logic

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    // Android Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,4 +48,17 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Picasso
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    // SQLite
+    val sqlite_version = "2.4.0"
+
+    // Kotlin
+    implementation("androidx.sqlite:sqlite-ktx:$sqlite_version")
 }
